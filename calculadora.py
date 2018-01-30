50391484e41b99cb4dbcffc8c7dc5f3993c19386
Juan Urena Garcia Gasco <jurenag@beta13.aulas.gsyc.urjc.es>
2018-01-30 12:57:36 +0100

    segunda prueba

--- calculadora.py
+++ calculadora.py
@@ -3,5 +3,11 @@
 import sys
 #tomar datos desde la ejecucion del programa
 
-print (sys.argv)
+from sys import argv
+#importa una funcion concreta para no tener que llamarla de sys 
+
+print (argv)
 print (sys.argv[2])
+
+
+sys.exit("El programa acaba")
