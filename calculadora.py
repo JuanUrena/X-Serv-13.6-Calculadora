1fc382e30cf8471b8233f9b9ab166b6190043f46
juan <juanurenagarciagasco@gmail.com>
2018-02-06 00:41:26 +0100

    Listo para entregar

--- calculadora.py
+++ calculadora.py
@@ -4,21 +4,31 @@ import sys
 from sys import argv
 # importa una funcion concreta para no tener que llamarla de sys
 # Obtengo los datos desde la linea de comandos
-operacion = (argv[1])
-num1 = float((argv[2]))
-num2 = float((argv[3]))
-# Compruebo el valor de la operacion para llevarla acabo
 try:
+    operacion = (argv[1])
+    num1 = float((argv[2]))
+    num2 = float((argv[3]))
+# Compruebo el valor de la operacion para llevarla acabo
+
     if operacion == 'suma':
         Resultado = num1+num2
+        print(Resultado)
     elif operacion == 'resta':
         Resultado = num1-num2
+        print(Resultado)
     elif operacion == 'multiplicacion':
         Resultado = num1*num2
+        print(Resultado)
     elif operacion == 'division':
         Resultado = num1/num2
+        print(Resultado)
     else:
-        print('caso especial consultar a Pacheco')
-    print(Resultado)
+        print('Operacón no detectada, pruebe con "suma", "resta", "multiplicacion" o "division"')
+    
 except ZeroDivisionError:
-    print("no se puede dividir por cero")
+    print("Error: No se puede dividir entre cero")
+except IndexError:
+    print('Error: La entrada debe ser: python3 calculadora.py <operación> <num1> <num2>') 
+except ValueError:
+    print('Error: La entrada debe ser: python3 calculadora.py <operación> <num1> <num2>') 
+    
