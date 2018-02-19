e505b27a96ceed473c5b87ed35fcb4f0536cc5d8
Juan Urena Garcia Gasco <jurenag@alpha38.aulas.gsyc.urjc.es>
2018-02-19 14:30:07 +0100

    Cambio de funciones

--- calculadora.py
+++ calculadora.py
@@ -5,32 +5,45 @@ import sys
 from sys import argv
 # importa una funcion concreta para no tener que llamarla de sys
 # Obtengo los datos desde la linea de comandos
-try:
-    operacion = (argv[1])
-    num1 = float((argv[2]))
-    num2 = float((argv[3]))
+
 # Compruebo el valor de la operacion para llevarla acabo
 
-    if operacion == 'suma':
-        Resultado = num1+num2
-        print(Resultado)
-    elif operacion == 'resta':
-        Resultado = num1-num2
-        print(Resultado)
-    elif operacion == 'multiplicacion':
-        Resultado = num1*num2
-        print(Resultado)
-    elif operacion == 'division':
-        Resultado = num1/num2
-        print(Resultado)
-    else:
-        print('Operacón no detectada')
-        print('Pruebe con "suma", "resta", "multiplicacion" o "division"')
-except ZeroDivisionError:
-    print("Error: No se puede dividir entre cero")
-except IndexError:
-    print('Error: La entrada debe ser:')
-    print('python3 calculadora.py <operación> <num1> <num2>')
-except ValueError:
-    print('Error: La entrada debe ser:')
-    print('python3 calculadora.py <operación> <num1> <num2>')
+def suma(op1,op2):
+    return num1+num2
+        
+def resta(op1,op2):
+    return num1-num2
+
+def multi(op1,op2):
+    return op1*op2       
+
+def div(op1,op2):
+    try:
+        return op1/op2
+        
+    except ZeroDivisionError:
+        return("Error: No se puede dividir entre cero")
+
+funciones ={
+   "suma": suma, 
+   "resta": resta, 
+   "multiplicacion":multi,
+   "division": div,
+}
+    
+if __name__ == "__main__":
+
+    try:
+        operacion = (argv[1])
+        num1 = float((argv[2]))
+        num2 = float((argv[3]))
+        print(str(funciones[operacion](num1,num2)))
+    except IndexError:
+        print('Error: La entrada debe ser:')
+        exit('python3 calculadora.py <operación> <num1> <num2>')
+    except ValueError:
+        print('Error: La entrada debe ser:')
+        exit('python3 calculadora.py <operación> <num1> <num2>')
+    except KeyError:
+        print('Operacion no detectada, pruebe con:')
+        print('suma, resta, multiplicacion, division')
